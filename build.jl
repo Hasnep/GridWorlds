f6b3892fd6f70cfe75bce5d749070c13f077ee6a
Hannes <hasnep@gmail.com>
2021-02-21 21:09:19 +0000

    Move build path

--- build.jl
+++ build.jl
@@ -1,4 +1,8 @@
 import Literate
-# using CliffWorlds
-Literate.markdown(joinpath(pwd(), "blogpost.jl"); documenter = false, execute = true)
-run(Cmd(`pandoc blogpost.md --output=build/blogpost.html`))
+using CliffWorlds
+mkpath(joinpath(pwd(), "build", "images"))
+Literate.markdown(joinpath(pwd(), "blogpost.jl"),joinpath(pwd(), "build"); documenter = false, execute = true)
+run(Cmd(`pandoc build/blogpost.md --output=build/blogpost.html`))
+
+# import Pandoc
+# Pandoc.parse_file(joinpath(pwd(),"build","blogpost.md"))
